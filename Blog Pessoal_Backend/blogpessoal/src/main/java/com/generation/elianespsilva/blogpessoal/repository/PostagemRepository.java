d0f86f40794195b8f4deb6c76c538045025b7447
= <3lian3.pereira@gmail.com>
2022-10-03 15:34:52 -0300

    ATIVIDADE CRUD BACKEND BLOG-PESSOAL – PARTE 02

--- Blog Pessoal_Backend/blogpessoal/src/main/java/com/generation/elianespsilva/blogpessoal/repository/PostagemRepository.java
+++ Blog Pessoal_Backend/blogpessoal/src/main/java/com/generation/elianespsilva/blogpessoal/repository/PostagemRepository.java
@@ -1,6 +1,9 @@
 package com.generation.elianespsilva.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.elianespsilva.blogpessoal.model.Postagem;
@@ -8,4 +11,6 @@ import com.generation.elianespsilva.blogpessoal.model.Postagem;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
+	public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo);
+	//esse não é um método padrão então deve ser feito no repository.
 }
